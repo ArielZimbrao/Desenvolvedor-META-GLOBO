4c1cda60026e49a698733d9434f7489b40ba7fa3
ArielZimbrao <aquilazimbrao@hotmail.com>
2021-04-24 17:57:49 -0300

    front-login

---
+++ frontend/src/components/Input/Input.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="input-container">
+        <input 
+            :type="type" 
+            class="field" 
+            :placeholder="label" 
+            :name="name" 
+            :id="name" 
+            :required="required" 
+            v-model="value"
+            @input="handleInput"
+        />
+        <label for="name" class="label">{{label}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: {
+        type: {
+            type: String,
+            default: 'text'
+        },
+        name: {
+            type: String,
+            default: 'field'
+        },
+        label: {
+            type: String,
+            default: 'Label'
+        },
+        value: {
+            type: String,
+            default: '',
+        },
+        required: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    data () {
+        return {
+            content: this.value
+        }
+    },
+    methods: {
+        handleInput (event) {
+            this.$emit('input', event.currentTarget.value);
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+    @import './Input.scss'
+</style>
---
+++ frontend/src/views/Login/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-background">
+    <div class="login-container">
+      <div class="logo-container">
+        <img class="logo" :src="logo" />
+      </div>
+      <form class="form-container">
+        <Input 
+          type="email"
+          name="email" 
+          label="Email" 
+          :value="email" 
+          v-model="email"
+          :required="true"
+        />
+        <Input 
+          type="password"
+          name="password" 
+          label="Password" 
+          :value="password" 
+          v-model="password"
+          :required="true"
+        />
+        <button class="button-signin" type="submit" @click="login">Sign in</button> 
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from '@/components/Input/Input.vue';
+import Logo from '@/assets/logo.png'
+
+export default {
+  name: 'Login',
+  components: {
+    Input
+  },
+  data: () => {
+    return {
+      email: '',
+      password: '',
+      logo: Logo
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.email)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  @import './Login.scss';
+</style>
